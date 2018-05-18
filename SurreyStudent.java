ff843352edb8d69ea8c28089896c9a800ff58a87
Meher Khan <33574476+meher007@users.noreply.github.com>
2018-05-18 16:35:28 +0100

    JSON parse java

---
+++ SurreyStudent.java
@@ -0,0 +1,32 @@
+package json.domain;
+
+public class SurreyStudent {
+	
+	
+	
+		
+		private long id;
+		private String name;
+		private String lastname;
+		
+		public long getId() {
+			return id;
+		}
+		public void setId(long id) {
+			this.id = id;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getLastname() {
+			return lastname;
+		}
+		public void setLastname(String lastname) {
+			this.lastname = lastname;
+		}
+
+
+}
